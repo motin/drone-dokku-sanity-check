ecd9ff0075f78435df7a0aff9ba887b035e137bf
Fredrik Wollsén <fredrik@neam.se>
2014-04-22 17:02:24 +0300

    Initial configuration of dokku sanity check drone configuration

---
+++ drone.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# enable debug
+set -x
+
+# fail on any error
+set -o errexit
+
+echo "===== Prepare some sample repository to deploy with dokku ====="
+mkdir /tmp/empty-git
+cd /tmp/empty-git
+git init
+git checkout -b master
+touch index.php
+git add index.php
+echo 'echo Hello' > shell-script.sh
+chmod +x shell-script.sh
+git add shell-script.sh
+git commit -m "Dokku sanity check commit"
+echo "Done"
+echo
+echo "===== Check access to dokku ===="
+git push dokku@$DOKKU_HOST:sanity-check master:master -f
+echo "Done"
+echo
+echo "===== Check output of remote dokku commands ===="
+ssh dokku@$DOKKU_HOST run sanity-check /bin/bash /app/shell-script.sh
+echo "Done"
+
+exit 0
